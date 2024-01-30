2da0796ef053d0161c7184f34a0c3ff796c469f6
Ham <ham97@mac.com>
2024-01-29 20:48:35 -0500

    adding filters and proposal comment type

--- src/researchhub_comment/filters.py
+++ src/researchhub_comment/filters.py
@@ -20,6 +20,7 @@ BEST = "BEST"
 TOP = "TOP"
 BOUNTY = "BOUNTY"
 REVIEW = "REVIEW"
+PROPOSAL = "PROPOSAL"
 DISCUSSION = "DISCUSSION"
 REPLICABILITY_COMMENT = "REPLICABILITY_COMMENT"
 CREATED_DATE = "CREATED_DATE"
@@ -41,6 +42,7 @@ PRIVACY_CHOICES = (
 FILTER_CHOICES = (
     (BOUNTY, "Has Bounty"),
     (REVIEW, REVIEW),
+    (PROPOSAL, PROPOSAL),
     (DISCUSSION, DISCUSSION),
     (REPLICABILITY_COMMENT, REPLICABILITY_COMMENT),
     (INNER_CONTENT_COMMENT, INNER_CONTENT_COMMENT),
@@ -180,6 +182,8 @@ class RHCommentFilter(filters.FilterSet):
             )
         elif value == REVIEW:
             qs = qs.filter(comment_type=REVIEW)
+        elif value == PROPOSAL:
+            qs = qs.filter(comment_type=PROPOSAL)
         elif value == INNER_CONTENT_COMMENT:
             qs = qs.filter(comment_type=INNER_CONTENT_COMMENT)
         elif value == DISCUSSION:
